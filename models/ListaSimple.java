feb704418d1771ed4fd9f3fd65476fad08689a7d
Sandoval <gtkaiser29@gmail.com>
2025-02-24 21:41:34 -0600

    Código en listasimple

--- models/ListaSimple.java
+++ models/ListaSimple.java
@@ -1,5 +1,37 @@
 package models;
 
-public class ListaSimple {
-    
+import java.util.ArrayList;
+
+public class ListaSimple<T> {
+    private ArrayList<T> elementos;
+
+    public ListaSimple() {
+        this.elementos = new ArrayList<>();
+    }
+
+    public void addFirst(T elemento) {
+        elementos.add(0, elemento);
+    }
+
+    public T eliminarFirst() {
+        if (!elementos.isEmpty()) {
+            return elementos.remove(0);
+        }
+        return null;
+    }
+
+    public T getFirst() {
+        if (!elementos.isEmpty()) {
+            return elementos.get(0);
+        }
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return elementos.isEmpty();
+    }
+
+    public int size() {
+        return elementos.size();
+    }
 }
