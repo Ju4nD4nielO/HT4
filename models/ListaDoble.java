bb3806302e3d13eb4d0c07a5e8388386d46e6a8b
Sandoval <gtkaiser29@gmail.com>
2025-02-24 20:45:24 -0600

    Código en listaDoble

--- models/ListaDoble.java
+++ models/ListaDoble.java
@@ -1,5 +1,37 @@
 package models;
 
-public class ListaDoble {
-    
+import java.util.LinkedList;
+
+public class ListaDoble<T> {
+    private LinkedList<T> elementos;
+
+    public ListaDoble() {
+        this.elementos = new LinkedList<>();
+    }
+
+    public void addFirst(T elemento) {
+        elementos.addFirst(elemento);
+    }
+
+    public T eliminarFirst() {
+        if (!elementos.isEmpty()) {
+            return elementos.removeFirst();
+        }
+        return null;
+    }
+
+    public T getFirst() {
+        if (!elementos.isEmpty()) {
+            return elementos.getFirst();
+        }
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return elementos.isEmpty();
+    }
+
+    public int size() {
+        return elementos.size();
+    }
 }
